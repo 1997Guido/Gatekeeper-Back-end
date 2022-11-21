4313a00d824d1d08d61c1b7511381966f2439c49
Mike Vermeer <77960135+MikeCVermeer@users.noreply.github.com>
2022-11-21 11:44:21 +0100

    commit

--- gatekeeper_backend/api/models.py
+++ gatekeeper_backend/api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+import string
+import random
+
+def generate_unique_id():
+    length = 8
+    
+    while True:
+        uid = ''.join(random.choices(string.ascii_uppercase, k=length))
+        if Users.objects.filter(uid=uid).count() == 0:
+            break
+        
+    return uid
+
 
 # Create your models here.
+class Users(models.Model):
+    first_name = models.CharField(max_length=32, default="")
+    last_name = models.CharField(max_length=64, default="")
+    age = models.IntegerField((""))
+    date_created = models.DateField(auto_now_add=True)
+    admin = models.BooleanField(default=False)
+    
--- gatekeeper_backend/api/views.py
+++ gatekeeper_backend/api/views.py
@@ -7,3 +7,4 @@ from django.http import HttpResponse
 
 def main(request):
     return HttpResponse("<h1>Hello</h1>")
+
