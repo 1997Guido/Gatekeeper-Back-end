1e1ccd8b1e1282cddecb660922833210917f8fcf
DeGuido1997 <97097281@st.deltion.nl>
2022-11-24 11:44:09 +0100

    commit

--- gatekeeper_backend/api/functions/QrCodeGenerator.py
+++ gatekeeper_backend/api/functions/QrCodeGenerator.py
@@ -1,3 +1,3 @@
-def QrCodeGenerator():
-    outcomeFunction = 'QrCodeGenerator'
+def QrCodeGenerator(name, lastname, age):
+    outcomeFunction = f'dit is een qr code van {name} en die is {age} oud zijn achternaam is {lastname}'
     return (outcomeFunction)
