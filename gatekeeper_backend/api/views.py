be3948a5e41c05fd6fdadcbcf6474bbadaa30943
DeGuido1997 <97097281@st.deltion.nl>
2022-11-24 11:11:13 +0100

    URL Paths, Redirect to functions in Views
    Ik heb voor elke api 'pagina' een functie gemaakt. er word naar deze functie toe gelinked in views.py

--- gatekeeper_backend/api/views.py
+++ gatekeeper_backend/api/views.py
@@ -2,9 +2,26 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import UserSerializer
 from .models import Users
+from django.http import HttpResponse
+from .functions.RegisterLogic import RegisterLogic
+from .functions.ProfileLogic import ProfileLogic
+from .functions.QrCodeGenerator import QrCodeGenerator
+from . functions.QrCodeScanner import QrCodeScanner
 
 # Create your views here.
 
 class UsersView(generics.ListAPIView):
     queryset = Users.objects.all()
-    serializer_class = UserSerializer
+    serializer_def = UserSerializer
+
+def RegisterApi(request):
+    return HttpResponse(RegisterLogic())
+
+def QrCodeGeneratorApi(request):
+    return HttpResponse(QrCodeGenerator())
+
+def QrCodeScannerApi(request):
+    return HttpResponse(QrCodeScanner)
+
+def ProfileApi(request):
+    return HttpResponse(ProfileLogic())
