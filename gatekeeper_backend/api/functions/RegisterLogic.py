66dff8404a8aaf36d5b0343bc55c2f6e1640de0d
DeGuido1997 <97097281@st.deltion.nl>
2022-11-25 13:23:44 +0100

    test

--- gatekeeper_backend/api/functions/RegisterLogic.py
+++
@@ -1,9 +0,0 @@
-from .QrCodeGenerator import QrCodeGenerator
-
-
-
-def RegisterLogic():
-    name = 'guido'
-    QrCode = QrCodeGenerator()
-    outcomeFunction = 'RegisterLogic'
-    return (QrCode)
