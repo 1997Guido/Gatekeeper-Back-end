4351ba51efc7fb84e676b0581dcb93e36a4a15f7
DeGuido1997 <97097281@st.deltion.nl>
2022-12-02 12:30:44 +0100

    commit

--- gatekeeper_backend/api/urls.py
+++ gatekeeper_backend/api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from .views import UserProfileView
 from .views import RegisterApi
 from .views import QrCodeGeneratorApi
@@ -15,6 +15,4 @@ urlpatterns = [
     path('qrcodegeneratorapi', QrCodeGeneratorApi),
     path('qrcodescannerapi', QrCodeScannerApi),
     path('profileapi', ProfileApi),
-    path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls'))
 ]
--- gatekeeper_backend/gatekeeper_backend/urls.py
+++ gatekeeper_backend/gatekeeper_backend/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls'))
 ]
