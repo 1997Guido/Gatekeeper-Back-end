891821bac5069d8bf356e0efac93879c12418544
Gido1997 <101527757+Gido1997@users.noreply.github.com>
2022-12-13 20:14:06 +0100

    commit

--- gatekeeper_backend/api/models.py
+++ gatekeeper_backend/api/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-import string
-import random
-from django import forms
+from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
+
 
 #This is the model for our Database
 #This is a custom user model which inherits most of its fields from the default UserModel created by Django.
@@ -11,13 +10,15 @@ from django.contrib.auth.models import AbstractUser
 #The code below are the 'custom' fields i added to the default user model.
 #Like age,gender and QrUid. the QrUid is the field for the unique id generated for each user in QrCodeGenerator.py
 
+
 class UserProfile(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
     GENDER_CHOICES = (
-    ('M', 'Male'),
-    ('F', 'Female')
+    ('Male', 'Male'),
+    ('Female', 'Female'),
+    ('Undefined', 'Undefined'),
     )
-    age = models.PositiveIntegerField(null=False, default=0)
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default=False, null=False)
+    gender = models.CharField(max_length=15, choices=GENDER_CHOICES, default=False, null=False)
     QrUid = models.CharField(max_length=8,null=False, default=0)
     def __str__(self):
         return self.username
