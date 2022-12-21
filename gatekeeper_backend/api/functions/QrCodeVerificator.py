9549d89b164e69f202c5198a0aece3b16527ec38
Mike Vermeer <77960135+MikeCVermeer@users.noreply.github.com>
2022-12-21 12:22:23 +0100

    commit

--- gatekeeper_backend/api/functions/QrCodeVerificator.py
+++ gatekeeper_backend/api/functions/QrCodeVerificator.py
@@ -4,10 +4,18 @@ import json
 
 def QrCodeVerificator(user, request):
     uid = UserProfile.objects.get(pk=user["pk"]).QrUid
+    user = (
+    UserProfile.objects.filter(pk=request.user.pk)
+    .values(
+        "first_name",
+        "last_name",
+        "gender",
+        )
+        .first()
+    )
 
-    #qrdata = request.body.get("encryptedqrdata").decode("utf-8")
 
-    #qrdata = json.loads(request.body.decode("utf-8"))["encryptedqrdata"]
+    qrdata = json.loads(request.body.decode("utf-8"))["encryptedqrdata"]
     
     QrData = str(qrdata).encode('utf-8')
     key = "rTFB13nkI4mt76RMiJOpoNZS_aa5LUNyJIJ4BPlbPEY="
@@ -18,9 +26,14 @@ def QrCodeVerificator(user, request):
     QrDataDecrypted = QrDataDecrypted.decode('utf-8')
     
     if QrDataDecrypted == uid:
-        return (True, " ", uid)
+        check = "True"
+        notjsondata = {
+        "userdata": user,
+        "check": check
+        }
+        
+        jsondata = json.dumps(notjsondata)
+        
+        return (jsondata)
     else:
-        return (False, "", uid)
-    
-    #return (QrDataDecrypted, uid)
-    #return("response")
+        return (False)
