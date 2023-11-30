c95c93f02c1724a1fbb72b868bda1df9385277c4
Guido Erdtsieck <guidoerdtsieck@hotmail.nl>
2023-11-30 11:12:54 +0100

    fix11

--- gatekeeper_backend/gatekeeper_backend/settings.py
+++ gatekeeper_backend/gatekeeper_backend/settings.py
@@ -166,7 +166,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 STATIC_ROOT = str(BASE_DIR / "staticfiles")
-STATIC_URL = "static/"
+STATIC_URL = "staticfiles/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
