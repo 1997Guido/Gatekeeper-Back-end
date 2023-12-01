fc26ae299bb57ce54bf20085a8e58b0b29f3b548
Guido Erdtsieck <guidoerdtsieck@hotmail.nl>
2023-12-01 11:47:05 +0100

    fix10

--- gatekeeper_backend/gatekeeper_backend/urls.py
+++ gatekeeper_backend/gatekeeper_backend/urls.py
@@ -24,3 +24,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG == False:
+    urlpatterns = "django/" + urlpatterns
